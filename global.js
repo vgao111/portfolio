b1244183ff573c54bae82b94afd84a41e2c59ac2
Vincent Gao <gaovincent987@gmail.com>
2026-04-16 16:15:00 -0700

    2.2

--- global.js
+++ global.js
@@ -3,3 +3,8 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+
+let navLinks = $$("nav a");
+
+let currlink = navLinks.find((a) => a.host === location.host && a.pathname === location.pathname)
