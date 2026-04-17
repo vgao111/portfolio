7fd46f82d818932f67a4f66380bd9f89e235109a
Vincent Gao <gaovincent987@gmail.com>
2026-04-16 17:48:45 -0700

    4.2

--- global.js
+++ global.js
@@ -42,3 +42,18 @@ if (a.host !== location.host) {
 
 nav.append(a);
 }
+
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+  `
+);
