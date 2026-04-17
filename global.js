50a88bdc2dab1371f587cc1ad1c711d16f1564da
Vincent Gao <gaovincent987@gmail.com>
2026-04-16 17:13:50 -0700

    final 3.2

--- global.js
+++ global.js
@@ -28,7 +28,16 @@ for (let p of pages) {
   }
 
 let a = document.createElement('a');
+
 a.href = url;
 a.textContent = title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+if (a.host !== location.host) {
+  a.target = "_blank";
+}
+
 nav.append(a);
 }
