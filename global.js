bbd13e97a0548e558230fdf9044ae843e5bb117f
Vincent Gao <gaovincent987@gmail.com>
2026-04-16 18:00:46 -0700

    4.4

--- global.js
+++ global.js
@@ -58,6 +58,9 @@ document.body.insertAdjacentHTML(
   `
 );
 
+let select = document.querySelector('.color-scheme select');
+
 select.addEventListener('input', function (event) {
   console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
 });
