e7094e90ad87650dfa645d67e8a9aeab1d0fd950
Vincent Gao <gaovincent987@gmail.com>
2026-04-21 18:38:59 -0700

    1.5

--- global.js
+++ global.js
@@ -84,8 +84,30 @@ export async function fetchJSON(url) {
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
+
+    const data = await response.json();
+    return data;
     
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
+
+
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+
+  for (let project of projects) {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  }
+} 
+
