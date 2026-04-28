acf289a664a8f1970e31cac2a82acd7f17bc07ae
Vincent Gao <gaovincent987@gmail.com>
2026-04-27 18:04:20 -0700

    2.2

--- projects/projects.js
+++ projects/projects.js
@@ -16,10 +16,17 @@ let arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(50);
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 
 
@@ -34,4 +41,13 @@ arcs.forEach((arc, idx) => {
     .append('path')
     .attr('d', arc)
     .attr('fill', colors(idx));
+});
+
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 });
