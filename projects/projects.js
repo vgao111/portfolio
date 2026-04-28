1b1ffb3649102ff60beef4bb835961b5c50bbff7
Vincent Gao <gaovincent987@gmail.com>
2026-04-27 17:32:39 -0700

    1.4

--- projects/projects.js
+++ projects/projects.js
@@ -19,3 +19,31 @@ let arc = arcGenerator({
   endAngle: 2 * Math.PI,
 });
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+
+let data = [1, 2];
+
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+
+arcs.forEach((arc) => {
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
+});
