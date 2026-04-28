cf0c48cdf5e2035dfdc414eafad696f91b9167b2
Vincent Gao <gaovincent987@gmail.com>
2026-04-27 18:18:31 -0700

    step 3

--- projects/projects.js
+++ projects/projects.js
@@ -16,15 +16,16 @@ let arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(50);
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
 
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
