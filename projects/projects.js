a662b977e4b1089eb6098d425b0f39b403a38723
Vincent Gao <gaovincent987@gmail.com>
2026-04-27 17:24:30 -0700

    1.3

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm'; 
 
 const projects = await fetchJSON('../lib/projects.json');
 
@@ -9,3 +10,12 @@ const projectsTitle = document.querySelector('#projtitle');
 projectsTitle.textContent = `${projects.length} Projects `;
 
 renderProjects(projects, projectsContainer, 'h2');
+
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
